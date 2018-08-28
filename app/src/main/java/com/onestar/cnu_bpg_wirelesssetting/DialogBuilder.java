814e2934045322f3fc677508ad2122c212f906ba
onestar <gkswndus10@naver.com>
2018-08-28 23:23:05 +0900

    phase 4.
    Update dialog with checkboxes layout (will be deleted) and functions for
    event of dialogs.
    
    Yet, update views is not working well.. need to be debugged.
    However, sending command works well, and need to make code clear for all
    options.

--- app/src/main/java/com/onestar/cnu_bpg_wirelesssetting/DialogBuilder.java
+++ app/src/main/java/com/onestar/cnu_bpg_wirelesssetting/DialogBuilder.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Switch;
@@ -54,20 +55,20 @@ public class DialogBuilder implements DialogInterface.OnClickListener, DialogInt
     private void makeSwitcher(AlertDialog.Builder builder) {
         final ArrayList<String> mSelectedItems = new ArrayList();
         final View view = mInflater.inflate(R.layout.dialog_switch, null);
-        final Switch pressure1Switch = (Switch) view.findViewById(R.id.switch_pressure1);
-        final Switch pressure2Switch = (Switch) view.findViewById(R.id.switch_pressure2);
-        final Switch rgbSwitch = (Switch) view.findViewById(R.id.switch_rgb);
-        final Switch irySwitch = (Switch) view.findViewById(R.id.switch_iry);
-        final Switch accgyroSwitch = (Switch) view.findViewById(R.id.switch_accgyro);
-        final Switch timestampSwitch = (Switch) view.findViewById(R.id.switch_timestamp);
 
-        // set switch on/off using valueManager.getter
+        //TODO: set default
 
         builder.setView(view)
                 .setTitle("ON/OFF the elements")
                 .setPositiveButton(R.string.yes_dialog, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
+                        Switch pressure1Switch = (Switch) view.findViewById(R.id.switch_pressure1);
+                        Switch pressure2Switch = (Switch) view.findViewById(R.id.switch_pressure2);
+                        Switch rgbSwitch = (Switch) view.findViewById(R.id.switch_rgb);
+                        Switch irySwitch = (Switch) view.findViewById(R.id.switch_iry);
+                        Switch accgyroSwitch = (Switch) view.findViewById(R.id.switch_accgyro);
+                        Switch timestampSwitch = (Switch) view.findViewById(R.id.switch_timestamp);
 
                         passParameters(pressure1Switch.isChecked() + "", pressure2Switch.isChecked() + "",
                                 rgbSwitch.isChecked() + "", irySwitch.isChecked() + "",
@@ -75,24 +76,42 @@ public class DialogBuilder implements DialogInterface.OnClickListener, DialogInt
 
                         dialog.dismiss();
                     }
-                })
-                .setNegativeButton(R.string.no_dialog, new DialogInterface.OnClickListener() {
+                });
+    }
+
+    private void makeNumberPickers(AlertDialog.Builder builder) {
+        final View view = mInflater.inflate(R.layout.dialog_numpicks, null);
+
+        //TODO: set default
+
+        builder.setView(view)
+                .setTitle("Set mA of each LED")
+                .setPositiveButton(R.string.yes_dialog, new DialogInterface.OnClickListener() {
+                    @Override
                     public void onClick(DialogInterface dialog, int id) {
+                        NumberPicker redPicker = (NumberPicker) view.findViewById(R.id.numberPicker_red);
+                        NumberPicker greenPicker = (NumberPicker) view.findViewById(R.id.numberPicker_green);
+                        NumberPicker bluePicker = (NumberPicker) view.findViewById(R.id.numberPicker_blue);
+                        NumberPicker yellowPicker = (NumberPicker) view.findViewById(R.id.numberPicker_yellow);
+                        NumberPicker irPicker = (NumberPicker) view.findViewById(R.id.numberPicker_ir);
+
+                        passParameters(redPicker.getValue() + "", greenPicker.getValue() + "",
+                                bluePicker.getValue() + "", yellowPicker.getValue() + "",
+                                irPicker.getValue() + "");
+
                         dialog.dismiss();
                     }
                 });
     }
 
-    private void makeNumberPickers(AlertDialog.Builder builder) {
-        // set led
-    }
-
     private void makeNumberPicker(AlertDialog.Builder builder) {
-        final View view = mInflater.inflate(R.layout.dialog_num, null);
+        final View view = mInflater.inflate(R.layout.dialog_numpick, null);
 
         final NumberPicker np = (NumberPicker) view.findViewById(R.id.numberPicker1);
         np.setWrapSelectorWheel(false);
         np.setOnValueChangedListener(this);
+
+        //TODO: set default
 
         if (mKey.equals(mContext.getResources().getString(R.string.reboot))) {
             np.setMaxValue(60); // 1min
@@ -104,24 +123,27 @@ public class DialogBuilder implements DialogInterface.OnClickListener, DialogInt
             builder.setTitle("Input FREQUENCY ( ~ )Hz");
         }
 
-        builder.setNegativeButton(mContext.getResources().getString(R.string.no_dialog), this)
-                .setPositiveButton(mContext.getResources().getString(R.string.yes_dialog), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        passParameters(np.getValue() + "");
-                        dialog.dismiss();
-                    }
-                });
+        builder.setPositiveButton(mContext.getResources().getString(R.string.yes_dialog), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                passParameters(np.getValue() + "");
+                dialog.dismiss();
+            }
+        });
     }
 
     private void makeEditText(AlertDialog.Builder builder) {
+        //TODO: set default
+
         if (mKey.equals(mContext.getResources().getString(R.string.wifi))) {
             final View view = mInflater.inflate(R.layout.dialog_text, null);
             builder.setView(view)
-                    .setMessage("Input Wi-Fi information")
+                    .setTitle("Input Wi-Fi information")
                     .setPositiveButton(R.string.yes_dialog, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
+                            //TODO: TextView1
+                            //TODD: TextView2
                             EditText editText1 = (EditText) view.findViewById(R.id.edit_text_1);
                             EditText editText2 = (EditText) view.findViewById(R.id.edit_text_2);
 
@@ -129,16 +151,11 @@ public class DialogBuilder implements DialogInterface.OnClickListener, DialogInt
 
                             dialog.dismiss();
                         }
-                    })
-                    .setNegativeButton(R.string.no_dialog, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.dismiss();
-                        }
                     });
         } else if (mKey.equals(mContext.getResources().getString(R.string.protocol))) {
             final View view = mInflater.inflate(R.layout.dialog_text, null);
             builder.setView(view)
-                    .setMessage("Choose protocol and input port number (1 ~ 65535)")
+                    .setTitle("Choose protocol and input port number (1 ~ 65535)")
                     .setPositiveButton(R.string.yes_dialog, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
@@ -149,22 +166,28 @@ public class DialogBuilder implements DialogInterface.OnClickListener, DialogInt
 
                             dialog.dismiss();
                         }
-                    })
-                    .setNegativeButton(R.string.no_dialog, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            dialog.dismiss();
-                        }
                     });
         }
     }
 
     private void makeChecker(AlertDialog.Builder builder) {
-        // report
-        // four check options
+        final String[] reportList = {"console", "nw", "both", "none"}; //TODO: enum?
+
+        //TODO: set default
+
+        builder.setSingleChoiceItems(R.array.report_to, 0, null)
+                .setPositiveButton(R.string.yes_dialog, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int position = ((AlertDialog)dialog).getListView().getCheckedItemPosition();
+                        passParameters(reportList[position] + "");
+                        dialog.dismiss();
+                    }
+                });
     }
 
     private void makeTimePicker(AlertDialog.Builder builder) {
-        // set time
+        // TODO: set time
     }
 
     private void passParameters(String... args) {
